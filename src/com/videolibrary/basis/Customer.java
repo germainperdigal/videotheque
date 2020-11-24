c562cb0919c41ff498d14451fca1070ac4796ff6
Germain PERDIGAL <perdigalgermain@gmail.com>
2020-11-24 15:38:38 +0100

    Customer basis

--- src/com/videolibrary/basis/Customer.java
+++ src/com/videolibrary/basis/Customer.java
@@ -1,4 +1,21 @@
 package com.videolibrary.basis;
 
 public class Customer {
+    public String fname;
+    public String lname;
+    public String email;
+
+    public void Customer(String fname, String lname, String email) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+    }
+
+    public void getCustomer() {
+        System.out.println("--- Client "+ this.fname +" "+this.lname+" ---");
+        System.out.println("Prénom : "+this.fname);
+        System.out.println("Nom : "+this.lname);
+        System.out.println("Adresse e-mail : "+this.email);
+    }
+
 }
