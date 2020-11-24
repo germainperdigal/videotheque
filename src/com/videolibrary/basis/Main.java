8c744f65a236844efcb1e94fffc31df91a76655a
Germain Perdigal <30522825+germainperdigal@users.noreply.github.com>
2020-11-24 17:00:56 +0100

    Delete Main.java

--- src/com/videolibrary/basis/Main.java
+++
@@ -1,9 +0,0 @@
-package com.videolibrary.basis;
-
-public class Main {
-    public static void main(String[] args) {
-        Customer myCustomer = new Customer("test", "test1", "test@test1.fr");
-
-        myCustomer.getCustomer();
-    }
-}
