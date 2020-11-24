f7e9143b94c99de661061f03f5bf74259716a33d
Thomas F <sheepbild@gmail.com>
2020-11-24 16:06:06 +0100

    Main init & Videotheque init

---
+++ src/com/videolibrary/basis/Main.java
@@ -0,0 +1,7 @@
+package com.videolibrary.basis;
+
+public class Main {
+    public void main() {
+        Videolibrary newVideolibrary = new Videolibrary("La vidéoluthèque de Germouille");
+    }
+}
--- src/com/videolibrary/basis/Videolibrary.java
+++ src/com/videolibrary/basis/Videolibrary.java
@@ -1,4 +1,15 @@
 package com.videolibrary.basis;
 
 public class Videolibrary {
+    private String title;
+    private Movie[] movies;
+    private Customer[] customers;
+
+    Videolibrary(String title) {
+        this.title = title;
+    }
+
+    public void showPrincipalMenu() {
+
+    }
 }
